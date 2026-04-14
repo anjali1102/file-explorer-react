4d8f62cd683e9cb3de9f082eabb36bf486944a94
anjali1102 <ac.chauhan1102@gmail.com>
2026-04-15 00:04:57 +0530

    [Feat]: Setup & Sidebar

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Sidebar from './components/Sidebar/Sidebar'
+
+function App() {
+
+  return (
+    <>
+      <section>
+        <Sidebar />
+      </section>
+    </>
+  )
+}
+
+export default App
