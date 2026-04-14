da586e870220ff7718836a1f4927013d98e924e0
anjali1102 <ac.chauhan1102@gmail.com>
2026-04-15 02:27:55 +0530

    [Feat]: Added data json and displayed on Sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,4 +1,16 @@
+import { useState } from 'react'
+import data from '../../data/data.json'
+
 function Sidebar() {
+    const [isOpen, setIsOpen] = useState(true)
+
+    const root = data.root
+    const hasChildren = root.children && root.children.length > 0
+
+    function handleTreeOpen() {
+        setIsOpen((prev) => !prev)
+    }
+
     return (
         <div className="drawer lg:drawer-open">
             <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
@@ -21,16 +33,10 @@ function Sidebar() {
                         </li>
 
                         <li>
-                            <button className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Homepage">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path></svg>
-                                <span className="is-drawer-close:hidden">Homepage</span>
-                            </button>
-                        </li>
-
-                        <li>
-                            <button className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Settings">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M20 7h-9"></path><path d="M14 17H5"></path><circle cx="17" cy="17" r="3"></circle><circle cx="7" cy="7" r="3"></circle></svg>
-                                <span className="is-drawer-close:hidden">Settings</span>
+                            <button className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="root" onClick={handleTreeOpen}>
+                                {root.type === 'folder' ? '🗂️' : '📄'}
+                                <span className="is-drawer-close:hidden">{root.name}</span>
+                                {hasChildren && <span className="is-drawer-close:hidden ml-auto">{isOpen ? '▼' : '▶'}</span>}
                             </button>
                         </li>
                     </ul>
